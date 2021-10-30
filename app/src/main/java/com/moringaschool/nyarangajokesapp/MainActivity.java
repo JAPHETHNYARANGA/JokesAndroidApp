0258e18c5fd7f670ae3589203a6abf66c92d190d
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-30 12:02:09 +0300

    Created url for various joke categories

--- app/src/main/java/com/moringaschool/nyarangajokesapp/MainActivity.java
+++ app/src/main/java/com/moringaschool/nyarangajokesapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.moringaschool.nyarangajokesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -8,6 +10,7 @@ import android.os.Bundle;
 import android.widget.LinearLayout;
 
 import com.moringaschool.nyarangajokesapp.Adapters.CategoryAdapter;
+import com.moringaschool.nyarangajokesapp.fragments.Main;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,14 +28,22 @@ public class MainActivity extends AppCompatActivity {
         categories = new ArrayList<>();
 
         //insert cat names
-        categories.add("category 1");
-        categories.add("category 2");
-        categories.add("cat 3");
+        categories.add("Any");
+        categories.add("Programming");
+        categories.add("Misc");
+        categories.add("Dark");
+        categories.add("Pun");
+        categories.add("Spooky");
+        categories.add("Chrismas");
 
         catList = findViewById(R.id.catList);
         adapter = new CategoryAdapter(categories);
 
-        catList.setLayoutManager(new LinearLayoutManager(this, LinearLayout.HORIZONTAL, false));
+        catList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         catList.setAdapter(adapter);
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction().replace(R.id.fragment_container, new Main(getResources().getString(R.string.Url)+ "Any?amount=10"));
+        transaction.commit();
     }
 }
